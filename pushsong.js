e7b98d876a4c71322a3e747f7d8a6841007a6a89
Hervé LEMAI <hlemai@vekia.fr>
2020-04-12 17:04:14 +0200

    add function

--- pushsong.js
+++ pushsong.js
@@ -1,27 +1,68 @@
 var net = require('net');
 
-
 function pushSong(path) {
     var client = new net.Socket();
+    var ret=-1;
     client.connect(1234, '127.0.0.1', function() {
         console.log('Connected');
         //client.write("")
         client.write("queue.push "+path+"\n");
     });
 
-    client.on('connect',function() {
-        console.log('OnConnected');
-        client.push(("queue.push "+path);
-    });
-
     client.on('data', function(data) {
         console.log('Received: ' + data);
+        ret=data;
         client.destroy(); // kill client after server's response
     });
     
     client.on('close', function() {
         console.log('Connection closed');
     });
+    return data;
 }
+
+function getOnAirNumber() {
+    var client = new net.Socket();
+    var ret=-1;
+    client.connect(1234, '127.0.0.1', function() {
+        console.log('Connected');
+        //client.write("")
+        client.write("request.on_air");
+    });
+
+    client.on('data', function(data) {
+        console.log('Received: ' + data);
+        ret=data;
+        client.destroy(); // kill client after server's response
+    });
+    
+    client.on('close', function() {
+        console.log('Connection closed');
+    });
+    return ret;
+}
+
+function getMetadat(number) {
+    var client = new net.Socket();
+    var ret=-1;
+    client.connect(1234, '127.0.0.1', function() {
+        console.log('Connected');
+        //client.write("")
+        client.write("request.on_air");
+    });
+
+    client.on('data', function(data) {
+        console.log('Received: ' + data);
+        ret=data;
+        client.destroy(); // kill client after server's response
+    });
+    
+    client.on('close', function() {
+        console.log('Connection closed');
+    });
+    return ret;
+}
+
+
 
 pushSong("/home/pi/Music/flux.m4a");
