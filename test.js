fb9b766903e1e9470d343f6665120d5102a81a02
Hervé LEMAI <hlemai@vekia.fr>
2020-04-12 20:53:05 +0200

    rename module

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-var ps=require("./pushsong");
+var ps=require("./music");
 
-var url = "https://www.youtube.com/watch?v=k2hGmoWFzaA"
+var url = "https://www.youtube.com/watch?v=k2hGmoWFzaA";
+url = "https://www.youtube.com/watch?v=m_9RYQnPu34";
 //ps.getAndPushYoutubeSong("https://www.youtube.com/watch?v=nojgLrJ6RBU",id => {console.log("ADDED "+id);});
 ps.getAndPushYoutubeSong(url,
         id => {console.log("ADDED "+id);});
